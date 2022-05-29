4233433ad60d8cb341d90d9dbf48ddf5bade301b
sunlumos <735287480@qq.com>
2022-05-29 09:14:16 +0800

    创建hello world

---
+++ hello.java
@@ -0,0 +1,10 @@
+// 这是java的快速入门，演示java的开发步骤
+// Hello 是一个公共的类
+// public static void main(String[] args)表示一个主方法
+public class hello{
+    // 编写一个main方法
+    public static void main(String[] args){
+        // System.out.println输出
+        System.out.println("Hello World");
+    }
+}
