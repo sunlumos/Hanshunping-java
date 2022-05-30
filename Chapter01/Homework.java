27da00a8fa92c8696d9ff4c63533418b2efc2285
sunlumos <735287480@qq.com>
2022-05-30 15:23:25 +0800

    第一章作业

---
+++ Chapter01/Homework.java
@@ -0,0 +1,25 @@
+package Chapter01;
+
+public class Homework {
+    
+}
+// 输出helloworld
+class Homework01 {
+    public static void main(String[] args){
+       System.out.println("Hello world");
+    }
+}
+// 输出个人基本信息
+class Homework02 {
+    public static void main(String[] args) {
+        System.out.println("个人信息");
+    }
+}
+
+class Homework03 {
+    /* JDK,JRE,JVM关系
+    JDK = JRE + 开发工具
+    JRE = JVM + 核心类库
+    JDK > JRE > JVM
+    */
+}
