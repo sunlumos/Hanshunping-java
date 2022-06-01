db624a78a4afd19d1ea611dd5cfb93c4cebe4893
sunlumos <735287480@qq.com>
2022-06-01 17:53:31 +0800

    个人信息测试

---
+++ Chapter02/Var02.java
@@ -0,0 +1,18 @@
+package Chapter02;
+
+public class Var02 {
+    public static void main(String[] args) {
+        // 记录人的信息
+        int age = 20;
+        double score = 33.2;
+        String name = "jack";
+        char gender = '男';
+        System.out.println("个人信息如下：");
+        System.out.println("姓名：" + name);
+        System.out.println("年龄：" + age);
+        System.out.println("性别：" + gender);
+        System.out.println("成绩：" + score);
+
+    }
+    
+}
