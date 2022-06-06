c34b92de8ee7b42296c845f9e70cff496267df8a
sunlumos <735287480@qq.com>
2022-06-06 17:49:20 +0800

    布尔类型

---
+++ Chapter02/Boolean.java
@@ -0,0 +1,13 @@
+package Chapter02;
+
+public class Boolean {
+    public static void main(String[] args) {
+        // 布尔类型不可以0或者非0，只能是true和false
+        boolean isPass = true;
+        if(isPass = true){
+            System.out.println("考试通过，恭喜");
+        } else {
+            System.out.println("没有通过，下次努力");
+        }
+    }
+}
