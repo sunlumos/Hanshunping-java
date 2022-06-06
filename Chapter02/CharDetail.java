ca51991a1a7d4a7ad3919d839259ac7513b976ed
sunlumos <735287480@qq.com>
2022-06-06 10:10:44 +0800

    字符型细节

---
+++ Chapter02/CharDetail.java
@@ -0,0 +1,9 @@
+package Chapter02;
+
+public class CharDetail {
+    public static void main(String[] args) {
+        // ''是字符类型，""是字符串类型，不能用于char
+        // 字符本质是存储于计算机中的对应的码值
+        // ASCII表一共128个，实际上一个字节可以表示256个字符
+    }
+}
