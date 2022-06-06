57177c9dd85c616749a9b79692e412057a4c40bd
sunlumos <735287480@qq.com>
2022-06-06 08:55:36 +0800

    整型细节

---
+++ Chapter02/VarDeatail.java
@@ -0,0 +1,9 @@
+package Chapter02;
+
+public class VarDeatail {
+    public static void main(String[] args) {
+        int n1 = 1;
+        // int n2 = 1L; 会报错，计算机认为是long型变成int
+        long n3 = 3;
+    }
+}
