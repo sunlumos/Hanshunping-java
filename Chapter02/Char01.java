652453a64a5de09f60d826e894c2f7362f4c0be3
sunlumos <735287480@qq.com>
2022-06-06 09:48:45 +0800

    字符型

---
+++ Chapter02/Char01.java
@@ -0,0 +1,11 @@
+package Chapter02;
+
+public class Char01 {
+    public static void main(String[] args) {
+        char c1 = 'a';
+        char c2 = '韩';
+        char c3 = ' ';
+        char c4 = '/';
+        System.out.println(c1+c2+c3+c4);
+    }
+}
