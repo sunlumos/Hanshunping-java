d09d13a41cf017efb02ae4e24427295add88e6b1
sunlumos <735287480@qq.com>
2022-06-07 15:35:00 +0800

    关系运算符

---
+++ Chapter04/RelationalOperator.java
@@ -0,0 +1,17 @@
+package Chapter04;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 8;
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        boolean flag = a>b;
+        System.out.println(flag);
+
+    }
+}
