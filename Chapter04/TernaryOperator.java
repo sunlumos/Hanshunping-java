988bcceabf1451392e5e30819da55ab22a4f0700
sunlumos <735287480@qq.com>
2022-06-07 16:07:37 +0800

    三元运算符

---
+++ Chapter04/TernaryOperator.java
@@ -0,0 +1,10 @@
+package Chapter04;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = a > b ? a : b;
+        System.out.println(c);
+    }
+}
