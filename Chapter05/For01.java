1dae425c47043e84c28a40d88a2bcdc1a5c56600
sunlumos <735287480@qq.com>
2022-06-09 20:14:00 +0800

    for循环细节

--- Chapter05/For01.java
+++ Chapter05/For01.java
@@ -1,5 +1,11 @@
 package Chapter05;
-
+/*
+ * i变量只在for循环体中有效
+ * 如果你想在外面使用i，则需要在外面声明i
+ * for(;;){
+ *   xxx
+ * }  表示的是一个无限循环
+ */
 public class For01 {
     public static void main(String[] args) {
         // for(循环变量初始化;循环条件;循环变量迭代)
