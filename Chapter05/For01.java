8740ff59036e58a83badec98c31693b0857d69c0
sunlumos <735287480@qq.com>
2022-06-09 18:13:43 +0800

    for循环

---
+++ Chapter05/For01.java
@@ -0,0 +1,11 @@
+package Chapter05;
+
+public class For01 {
+    public static void main(String[] args) {
+        // for(循环变量初始化;循环条件;循环变量迭代)
+        // 循环操作
+         for (int i = 1; i <= 10; i++) {
+             System.out.println("hsp"+i);
+         }
+    }
+}
