a71672f90ceb71aa78025319f42259253beb1bd2
sunlumos <735287480@qq.com>
2022-06-09 15:57:23 +0800

    swich语句

---
+++ Chapter05/Switch.java
@@ -0,0 +1,17 @@
+package Chapter05;
+
+public class Switch {
+    public static void main(String[] args) {
+        int ch = 'b';
+        switch(ch) {
+            case 'a':
+            System.out.println("OK1");
+            break;
+            case 'b':
+            System.out.println("OK2");
+            break;
+            default :
+            System.out.println("OK3");
+        }
+    }
+}
