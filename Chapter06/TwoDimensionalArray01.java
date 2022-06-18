786b7e6692869df800471dceacf9a84a0aeadf80
sunlumos <735287480@qq.com>
2022-06-18 09:14:54 +0800

    二维数组的使用

--- Chapter06/TwoDimensionalArray01.java
+++ Chapter06/TwoDimensionalArray01.java
@@ -11,10 +11,15 @@ public class TwoDimensionalArray01 {
     };
 
     // 输出二维数组
+    // ! 二维数组关键概念
+    // 二维数组的每个元素都是一个一维数组，所以如果需要得到每个一维数组的值，还需要继续遍历
+    // 如果需要访问第（i + 1）个一维数组的第j+1个值，arr[i][j]
+    // 原因：从0开始计数
+    System.out.println("第三个一维数组的第四个值为：" + arr[2][3]);
     for (int i = 0; i < arr.length; i++) {
         // 遍历二维数组的每个元素（数组）
         // arr[i] 表示：二维数组的第i+1个元素
-        // 1. arr[i].length得到对应的每个一维数组的长度
+        // arr[i].length得到对应的每个一维数组的长度
         for (int j = 0; j < arr[i].length; j++) {
             System.out.print(arr[i][j] + "\t");
         }
