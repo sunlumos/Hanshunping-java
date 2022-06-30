fc967604ebdff4f2c4df8bd2599ce7b7af756ab4
sunlumos <735287480@qq.com>
2022-06-30 14:58:49 +0800

    方法重载细节

--- Chapter07/OverLoad01.java
+++ Chapter07/OverLoad01.java
@@ -2,6 +2,11 @@ package Chapter07;
 
 public class OverLoad01 {
     // todo:方法重载，同名方法不同的接受参数，不同功能
+    // !注意点
+    // 1.方法名一定要相同
+    //! 2.形参的类型或个数或者顺序至少有一样不同，参数名不影响
+    // 3.返回类型对于方法重复没影响
+    
     public static void main(String[] args) {
         myCalculate mc = new myCalculate();
         System.out.println(mc.calculate(3, 4,5));
