75676c867f52597f0f629b07578e5437324ee855
sunlumos <735287480@qq.com>
2022-07-20 09:59:59 +0800

    限定符

---
+++ Chapter08/src/modifier/A.java
@@ -0,0 +1,13 @@
+package modifier;
+
+public class A {
+    // 属性
+    public int n1 = 100;
+    protected int n2 = 200;
+    int n3 = 300;
+    private int n4 = 400;
+    public void m1() {
+        // 在本类中，该方法可以访问四个属性
+        System.out.println("n1 = " + n1 + " n2 = " + n2 + " n3 = " + n3 + " n4 = " + n4);
+    }
+}
