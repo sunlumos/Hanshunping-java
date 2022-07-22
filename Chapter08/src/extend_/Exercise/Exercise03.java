215ee530b32f1f220a3351af8b2bc8c1fb2cf9d5
sunlumos <735287480@qq.com>
2022-07-22 08:53:23 +0800

    练习题三

---
+++ Chapter08/src/extend_/Exercise/Exercise03.java
@@ -0,0 +1,14 @@
+package extend_.Exercise;
+
+public class Exercise03 {
+    
+}
+
+class Conputer {
+    String CPU;
+    String inside;
+    String pan;
+    public String getDetail(){
+        return "CPU:" + CPU + " inside:" + inside + " pan:" + pan; 
+    }
+}
