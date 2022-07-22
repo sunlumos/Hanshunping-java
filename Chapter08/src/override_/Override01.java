993012417e2f25c59af69247413418631107b341
sunlumos <735287480@qq.com>
2022-07-22 15:40:25 +0800

    方法重写入门

--- Chapter08/src/override_/Override01.java
+++ Chapter08/src/override_/Override01.java
@@ -4,7 +4,7 @@ public class Override01 {
     public static void main(String[] args) {
         //演示方法重写的情况
         Dog dog = new Dog();
-        dog.cry();//ctrl+b
+        dog.cry();  //小狗汪汪叫，执行的是子类的方法
     }
 }
 
