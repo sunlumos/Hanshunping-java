3eda520419b6b0c6fe71f50f8278924aba8e905e
sunlumos <735287480@qq.com>
2022-07-27 14:57:07 +0800

    多态属性细节

--- Chapter08/src/poly_/detail_/PolyDetail02.java
+++ Chapter08/src/poly_/detail_/PolyDetail02.java
@@ -2,7 +2,7 @@ package poly_.detail_;
 
 public class PolyDetail02 {
     public static void main(String[] args) {
-        //属性没有重写之说！属性的值看编译类型
+        //!属性没有重写之说！属性的值看编译类型
         Base base = new Sub();//向上转型
         System.out.println(base.count);// ？ 看编译类型 10
         Sub sub = new Sub();
--- Chapter08/src/poly_/detail_/PolyDetail03.java
+++ Chapter08/src/poly_/detail_/PolyDetail03.java
@@ -3,10 +3,12 @@ package poly_.detail_;
 public class PolyDetail03 {
     public static void main(String[] args) {
         BB bb = new BB();
+        // instanceof用于判断对象A的运行类型是否属于B同一类型或者子类
         System.out.println(bb instanceof  BB);// true
         System.out.println(bb instanceof  AA);// true
 
         //aa 编译类型 AA, 运行类型是BB
+        //! 编译类型看左，运行类型看右
         //BB是AA子类
         AA aa = new BB();
         System.out.println(aa instanceof AA);
